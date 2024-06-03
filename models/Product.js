1d41bf67662939ad48addb7956f3de2597a8722f
rafiqul-bashar <rafiqul.bashar@outlook.com>
2024-06-03 17:12:09 +0600

    fixed deployment

--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  category: String,
-  // Add more fields as needed
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Product", ProductSchema);
